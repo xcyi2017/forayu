ebce031d9af102cfae73a5abb32f65e7b37caec6
Kofi sarfo <kofi@wimiro.com>
2014-04-02 10:18:42 +0100

    Extract household and household tests

--- Wimicrogrid/ApplianceTests.cs
+++ Wimicrogrid/ApplianceTests.cs
@@ -1,81 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
 using NUnit.Framework;
 
 namespace Wimicrogrid
 {
-    [TestFixture]
-    public class HouseHoldTests
-    {
-        [Test]
-        public void Should_consume_more_power_when_another_appliance_switched_on_is_added()
-        {
-            var anHour = new TimeSpan(1, 0, 0);
-            var clock = new Clock(anHour);
-            var television = new Appliance(clock, new Rating(3), ApplianceState.On);
-            var radio = new Appliance(clock, new Rating(1), ApplianceState.On);
-            var initialAppliances = new List<Appliance> {television};
-
-            var household1 = new Household(initialAppliances);
-            var household2 = new Household(initialAppliances);
-
-            clock.Tick();
-
-            household1.AddAppliance(radio);
-
-            clock.Tick();
-
-            Assert.That(household1.Usage, Is.GreaterThan(household2.Usage));
-        }
-
-        [Test]
-        public void Should_consume_no_power_when_all_appliances_are_switched_off()
-        {
-            var anHour = new TimeSpan(1, 0, 0);
-            var clock = new Clock(anHour);
-            var television = new Appliance(clock, new Rating(3), ApplianceState.Off);
-            var kettle = new Appliance(clock, new Rating(1), ApplianceState.Off);
-            var radio = new Appliance(clock, new Rating(1), ApplianceState.Off);
-            var initialAppliances = new List<Appliance> { television };
-
-            var household1 = new Household(initialAppliances);
-            clock.Tick();
-
-            household1.AddAppliance(radio);
-            clock.Tick();
-
-            household1.AddAppliance(kettle);
-            clock.Tick();
-
-            Assert.That(household1.Usage, Is.EqualTo(Consumption.None));
-        }
-    }
-
-    public class Household
-    {
-        private readonly List<Appliance> _appliances = new List<Appliance>();
-
-        public Household(IEnumerable<Appliance> appliances)
-        {
-            foreach (var appliance in appliances)
-            {
-                _appliances.Add(appliance);
-            }
-        }
-
-        public double Usage
-        {
-            get {  return _appliances.Sum(appliance => appliance.Usage); }
-        }
-
-        public void AddAppliance(Appliance appliance)
-        {
-            _appliances.Add(appliance);
-        }
-    }
-
     [TestFixture]
     public class ApplianceTests
     {
@@ -146,6 +73,5 @@ namespace Wimicrogrid
 
             Assert.That(appliance.Usage, Is.EqualTo(new Consumption(new TimeSpan(0,2,0,0), rating).Amount));
         }
-
     }
 }
