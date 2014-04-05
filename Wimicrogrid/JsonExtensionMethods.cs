afd0ccf8e5e50297578ebef1066f859c29cfa770
Kofi sarfo <kofi@wimiro.com>
2014-04-05 14:42:31 +0100

    Add generic JSON serialisation for community + household

---
+++ Wimicrogrid/JsonExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Wimicrogrid
+{
+    public static class JsonExtensionMethods
+    {
+        public static string AsJson<T>(this T model)
+        {
+            return JsonConvert.SerializeObject(model);
+        }
+    }
+}
