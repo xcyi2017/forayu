0ab0bd204c9f9881af3778bf34057bc87907f5a0
Kofi sarfo <kofi@wimiro.com>
2014-04-05 14:43:30 +0100

    Expose household appliances + allow appliance removal

--- Wimicrogrid/Household.cs
+++ Wimicrogrid/Household.cs
@@ -28,9 +28,28 @@ namespace Wimicrogrid
             get {  return _appliances.Sum(appliance => appliance.Usage); }
         }
 
+        public Appliances Appliances
+        {
+            get { return _appliances; }
+        }
+
+        public IEnumerable<Appliance> MagicBasement
+        {
+            get { return _appliances.All; }
+        }
+
         public void AddAppliance(Appliance appliance)
         {
             _appliances.Add(appliance);
+        }
+
+        public void RemoveAppliance(string id)
+        {
+            var applianceToRemove = _appliances.SingleOrDefault(appliance => appliance.Id == id);
+
+            if(applianceToRemove == null) return;
+
+            _appliances.Remove(applianceToRemove);
         }
     }
 }
