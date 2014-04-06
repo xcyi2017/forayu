8bd228f688dc9d0d92851c109ba4bf4812fb8ca2
Kofi sarfo <kofi@wimiro.com>
2014-04-06 01:07:30 +0100

    Verify that different instance of default appliances are unique

---
+++ Wimicrogrid/AppliancesTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Moq;
+using NUnit.Framework;
+
+namespace Wimicrogrid
+{
+    [TestFixture]
+    public class AppliancesTests
+    {
+        [Test]
+        public void Should_give_different_appliance_instances_as_default()
+        {
+            var mockClock = new Mock<ITime>();
+            var appliances = new Appliances(mockClock.Object);
+            var firstDefaults = appliances.All;
+            var nextDefaults = appliances.All;
+
+            Assert.False(nextDefaults.Any(appliance => firstDefaults.Any(first => first.Id == appliance.Id)));
+        }
+    }
+}
