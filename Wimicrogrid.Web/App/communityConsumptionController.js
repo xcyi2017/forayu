9f0b2dbc141baab587e2d8a042dbb7ee75fc7ff6
Kofi sarfo <kofi@wimiro.com>
2014-04-09 23:39:09 +0100

    Add community consumption controller

---
+++ Wimicrogrid.Web/App/communityConsumptionController.js
@@ -0,0 +1,18 @@
+﻿function CommunityConsumptionController($scope, $http, $routeParams, $timeout)
+{
+    $scope.consumption = [];
+
+    var updateConsumption = function () {
+        $http.get("/api/consumption")
+            .success(function (data) {
+                $scope.consumption = data;
+            })
+            .error(function (data, status) {
+                $scope.status = status;
+            });
+
+        $timeout(updateConsumption, 1000);
+    };
+
+    updateConsumption();
+}
